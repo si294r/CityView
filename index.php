a8e89adc2ec86fe8475e119dac780f1ca09c4561
Chairuddinsyah <chairuddinsyah@10.0.4.13>
2017-05-12 13:42:42 +0700

    Update Commit

--- index.php
+++ index.php
@@ -54,7 +54,7 @@ $query_string = isset($_SERVER["QUERY_STRING"]) ? $_SERVER["QUERY_STRING"] : "";
 $params = explode("/", $query_string);
 
 $service = isset($params[0]) ? $params[0] : "";
-echo $service;
+var_dump($params);
 
 // validate service ...
 switch ($service) {
